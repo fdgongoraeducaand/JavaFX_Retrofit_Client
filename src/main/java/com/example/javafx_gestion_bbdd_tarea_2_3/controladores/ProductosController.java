74dbca846572eb538eedd6941d6106bf2ebf2188
fdgongora2 <fdgongora@iesmurgi.org>
2022-02-14 19:26:34 +0100

    Initial commit

---
+++ src/main/java/com/example/javafx_gestion_bbdd_tarea_2_3/controladores/ProductosController.java
@@ -0,0 +1,189 @@
+package com.example.javafx_gestion_bbdd_tarea_2_3.controladores;
+
+import com.example.javafx_gestion_bbdd_tarea_2_3.dao.ProductoDAO;
+import com.example.javafx_gestion_bbdd_tarea_2_3.modelos.Producto;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.converter.DoubleStringConverter;
+import javafx.util.converter.NumberStringConverter;
+
+import java.sql.SQLException;
+
+public class ProductosController {
+
+    private ProductoDAO productDAO = new ProductoDAO();
+    private Producto productoAux;
+    private ObservableList <Producto> datos;
+
+    @javafx.fxml.FXML
+    private TableColumn tcProductVendor;
+    @javafx.fxml.FXML
+    private Button btnBorrar;
+    @javafx.fxml.FXML
+    private TextField txtVendedor;
+    @javafx.fxml.FXML
+    private TextField txtStock;
+    @javafx.fxml.FXML
+    private TableColumn tcBuyPrice;
+    @javafx.fxml.FXML
+    private TableView<Producto> tvProductos;
+    @javafx.fxml.FXML
+    private TableColumn tcQuantityInStock;
+    @javafx.fxml.FXML
+    private TextField txtID;
+    @javafx.fxml.FXML
+    private TextField txtNombre;
+    @javafx.fxml.FXML
+    private TextField txtEscala;
+    @javafx.fxml.FXML
+    private TableColumn tcProductDescription;
+    @javafx.fxml.FXML
+    private TextField txtPrecioCompra;
+    @javafx.fxml.FXML
+    private TableColumn tcProductScale;
+    @javafx.fxml.FXML
+    private Button btnAlta;
+    @javafx.fxml.FXML
+    private TableColumn tcMSRP;
+    @javafx.fxml.FXML
+    private TextField txtPrecioVenta;
+    @javafx.fxml.FXML
+    private TextField txtLinea;
+    @javafx.fxml.FXML
+    private TableColumn tcProductName;
+    @javafx.fxml.FXML
+    private TableColumn tcProductLine;
+    @javafx.fxml.FXML
+    private TextField txtDescripcion;
+    @javafx.fxml.FXML
+    private Button btnActualizar;
+    @javafx.fxml.FXML
+    private TableColumn tcProductCode;
+
+    public void initialize()  {
+
+        cargarDatosTabla();
+        productoAux = new Producto("","","",
+                "","","",0,0d,0d);
+        realizarBindingsProductoAux(productoAux);
+        cargarGestorDobleCLick();
+    }
+
+    private void cargarDatosTabla () {
+        datos = productDAO.obtenerProductos();
+
+        tcProductCode.setCellValueFactory(new PropertyValueFactory<Producto, String>("productCode"));
+        tcProductDescription.setCellValueFactory(new PropertyValueFactory<Producto, String>("productDescription"));
+        tcProductLine.setCellValueFactory(new PropertyValueFactory<Producto, String>("productLine"));
+        tcProductName.setCellValueFactory(new PropertyValueFactory<Producto, String>("productName"));
+        tcProductScale.setCellValueFactory(new PropertyValueFactory<Producto, String>("productScale"));
+        tcProductVendor.setCellValueFactory(new PropertyValueFactory<Producto, String>("productVendor"));
+
+        tcBuyPrice.setCellValueFactory(new PropertyValueFactory<Producto, Double>("buyPrice"));
+        tcMSRP.setCellValueFactory(new PropertyValueFactory<Producto, Double>("MSRP"));
+        tcQuantityInStock.setCellValueFactory(new PropertyValueFactory<Producto, Integer>("quantityInStock"));
+
+        tvProductos.setItems(datos);
+    }
+
+    private void realizarBindingsProductoAux ( Producto producto) {
+
+        txtEscala.textProperty().bindBidirectional(productoAux.productScaleProperty());
+        txtDescripcion.textProperty().bindBidirectional(producto.productDescriptionProperty());
+        txtID.textProperty().bindBidirectional(producto.productCodeProperty());
+        txtLinea.textProperty().bindBidirectional(producto.productLineProperty());
+        txtNombre.textProperty().bindBidirectional(producto.productNameProperty());
+        txtPrecioCompra.textProperty().bindBidirectional(producto.buyPriceProperty(),new NumberStringConverter());
+        txtPrecioVenta.textProperty().bindBidirectional(producto.MSRPProperty(), new NumberStringConverter() );
+        txtStock.textProperty().bindBidirectional(producto.quantityInStockProperty(), new NumberStringConverter() );
+        txtVendedor.textProperty().bindBidirectional(producto.productVendorProperty());
+    }
+
+    // Detectar el doble click en la tabla y cargar los datos en los campos de edición
+    private void cargarGestorDobleCLick () {
+        tvProductos.setRowFactory(tv -> {
+            TableRow<Producto> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    productoAux.setProductCode(row.getItem().getProductCode());
+                    productoAux.setProductLine(row.getItem().getProductLine());
+                    productoAux.setProductName(row.getItem().getProductName());
+                    productoAux.setProductScale(row.getItem().getProductScale());
+                    productoAux.setProductVendor(row.getItem().getProductVendor());
+                    productoAux.setProductDescription(row.getItem().getProductDescription());
+                    productoAux.setBuyPrice(row.getItem().getBuyPrice());
+                    productoAux.setMSRP(row.getItem().getMSRP());
+                    productoAux.setQuantityInStock(row.getItem().getQuantityInStock());
+                }
+            });
+            return row;
+        });
+    }
+
+    @javafx.fxml.FXML
+    public void onActualizarClicked(ActionEvent actionEvent) {
+
+        if ( ! productoAux.getProductCode().trim().equals("")) {
+            if (productDAO.actualizarProducto(productoAux)) {
+                cargarDatosTabla();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "No se ha encontrado un producto con el código '"
+                         + productoAux.getProductCode() + "' .", ButtonType.OK );
+                alert.showAndWait();
+            }
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Debe indicar el código del producto a actualizar.", ButtonType.OK );
+            alert.showAndWait();
+        }
+
+
+
+    }
+
+    @javafx.fxml.FXML
+    public void onAltaClicked(ActionEvent actionEvent) {
+        if ( ! productoAux.getProductCode().trim().equals("")) {
+            if (productDAO.altaProducto(productoAux)) {
+                cargarDatosTabla();
+            }
+        }
+        else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Debe introducir un código de producto.", ButtonType.OK );
+                alert.showAndWait();
+            }
+
+
+    }
+
+    @javafx.fxml.FXML
+    public void onBorrarClicked(ActionEvent actionEvent) {
+
+        Alert alert;
+
+        if ( ! productoAux.getProductCode().trim().equals("")) {
+
+            alert = new Alert(Alert.AlertType.CONFIRMATION, "¿ Desea borrar el producto con el código '"
+                    + productoAux.getProductCode() + "' ?.", ButtonType.YES, ButtonType.NO );
+
+            alert.showAndWait();
+            if (alert.getResult() == ButtonType.YES) {
+
+                if (productDAO.borrarProducto(productoAux)) {
+                    cargarDatosTabla();
+                } else {
+                    alert = new Alert(Alert.AlertType.INFORMATION, "No se ha encontrado un producto con el código '"
+                            + productoAux.getProductCode() + "' .", ButtonType.OK );
+                    alert.showAndWait();
+                }
+            }
+        }
+        else {
+            alert = new Alert(Alert.AlertType.INFORMATION, "Debe indicar el código del producto a borrar.", ButtonType.OK );
+            alert.showAndWait();
+        }
+    }
+}
---
+++ src/main/java/com/example/javafx_gestion_bbdd_tarea_2_3/dao/ProductoDAO.java
@@ -0,0 +1,196 @@
+package com.example.javafx_gestion_bbdd_tarea_2_3.dao;
+
+import com.example.javafx_gestion_bbdd_tarea_2_3.modelos.Producto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.*;
+
+public class ProductoDAO {
+
+    private final String servidor = "jdbc:mariadb://localhost:5555/noinch?useSSL=false";
+    private final String usuario = "adminer";
+    private final String passwd = "adminer";
+
+    private Connection conexionBBDD;
+
+    public ObservableList<Producto> obtenerProductos() {
+
+        ObservableList<Producto> datosResultadoConsulta = FXCollections.observableArrayList();
+        try {
+            // Nos conectamos
+            conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
+            String SQL = "SELECT * "
+                    + "FROM products "
+                    + "ORDER By productName";
+
+            // Ejecutamos la consulta y nos devuelve una matriz de filas (registros) y columnas (datos)
+            ResultSet resultadoConsulta = conexionBBDD.createStatement().executeQuery(SQL);
+
+            // Debemos cargar los datos en el ObservableList (Que es un ArrayList especial)
+            // Los datos que devuelve la consulta no son directamente cargables en nuestro objeto
+            while (resultadoConsulta.next()) {
+                datosResultadoConsulta.add(new Producto(
+                        resultadoConsulta.getString("productCode"),
+                        resultadoConsulta.getString("productName"),
+                        resultadoConsulta.getString("productLine"),
+                        resultadoConsulta.getString("productScale"),
+                        resultadoConsulta.getString("productVendor"),
+                        resultadoConsulta.getString("productDescription"),
+                        resultadoConsulta.getInt("quantityInStock"),
+                        resultadoConsulta.getDouble("buyPrice"),
+                        resultadoConsulta.getDouble("MSRP"))
+                );
+                System.out.println("Row [1] added " + resultadoConsulta.toString());
+            }
+            conexionBBDD.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" + e.toString());
+            conexionBBDD.close();
+        } finally {
+            return datosResultadoConsulta;
+        }
+    }
+
+    // Alta de un nuevo producto
+    //
+    public Boolean altaProducto(Producto producto) {
+
+        int registrosAfectadosConsulta = 0;
+
+        try {
+            // Nos conectamos
+            conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
+            String SQL = "INSERT INTO products ("
+                    + " productCode ,"
+                    + " productName ,"
+                    + " productLine ,"
+                    + " productScale ,"
+                    + " productVendor ,"
+                    + " productDescription ,"
+                    + " quantityInStock ,"
+                    + " buyPrice ,"
+                    + " MSRP  )"
+                    + " VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement st = conexionBBDD.prepareStatement(SQL);
+            st.setString(1, producto.getProductCode());
+            st.setString(2, producto.getProductName());
+            st.setString(3, producto.getProductLine());
+            st.setString(4, producto.getProductScale());
+            st.setString(5, producto.getProductVendor());
+            st.setString(6, producto.getProductDescription());
+
+            st.setInt(7, producto.getQuantityInStock());
+            st.setDouble(8, producto.getBuyPrice());
+            st.setDouble(9, producto.getMSRP());
+
+            // Ejecutamos la consulta preparada (con las ventajas de seguridad y velocidad en el servidor de BBDD
+            // nos devuelve el número de registros afectados. Al ser un Insert nos debe devolver 1 si se ha hecho correctamente
+            registrosAfectadosConsulta = st.executeUpdate();
+            st.close();
+            conexionBBDD.close();
+
+            if (registrosAfectadosConsulta == 1) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" + e.toString());
+            return false;
+        }
+    }
+
+    // Actualizar un producto existente
+    //
+    public Boolean actualizarProducto(Producto producto) {
+
+        int registrosAfectadosConsulta = 0;
+
+        try {
+            // Nos conectamos
+            conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
+            String SQL = "UPDATE products "
+                    + " SET "
+                    + " productName = ? ,"
+                    + " productLine = ? ,"
+                    + " productScale = ? ,"
+                    + " productVendor = ? ,"
+                    + " productDescription = ? ,"
+                    + " quantityInStock = ? ,"
+                    + " buyPrice = ? ,"
+                    + " MSRP = ?  "
+                    + " WHERE productCode = ? ";
+
+            PreparedStatement st = conexionBBDD.prepareStatement(SQL);
+
+            st.setString(1, producto.getProductName());
+            st.setString(2, producto.getProductLine());
+            st.setString(3, producto.getProductScale());
+            st.setString(4, producto.getProductVendor());
+            st.setString(5, producto.getProductDescription());
+
+            st.setInt(6, producto.getQuantityInStock());
+            st.setDouble(7, producto.getBuyPrice());
+            st.setDouble(8, producto.getMSRP());
+
+            st.setString(9, producto.getProductCode());
+
+            // Ejecutamos la consulta preparada (con las ventajas de seguridad y velocidad en el servidor de BBDD
+            // nos devuelve el número de registros afectados. Al ser un Update nos debe devolver 1 si se ha hecho correctamente
+            registrosAfectadosConsulta = st.executeUpdate();
+            st.close();
+            conexionBBDD.close();
+
+            if (registrosAfectadosConsulta == 1) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" + e.toString());
+            return false;
+        }
+    }
+    // Actualizar un producto existente
+    //
+    public Boolean borrarProducto(Producto producto) {
+
+        int registrosAfectadosConsulta = 0;
+
+        try {
+            // Nos conectamos
+            conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
+            String SQL = "DELETE FROM products "
+                    + " WHERE productCode = ? ";
+
+            PreparedStatement st = conexionBBDD.prepareStatement(SQL);
+
+            st.setString(1, producto.getProductCode());
+
+            // Ejecutamos la consulta preparada (con las ventajas de seguridad y velocidad en el servidor de BBDD
+            // nos devuelve el número de registros afectados. Al ser un Delete nos debe devolver 1 si se ha hecho correctamente
+            registrosAfectadosConsulta = st.executeUpdate();
+            st.close();
+            conexionBBDD.close();
+
+            if (registrosAfectadosConsulta == 1) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" + e.toString());
+            return false;
+        }
+    }
+}
+
